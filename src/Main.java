d540aaa7083548a9f7f7ebbdc16ca7759a63159e
wilso3rc <wilso3rc@mail.uc.edu>
2023-06-06 21:08:43 -0400

    final with variables and outputs

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int creditCardBalance = 5000;
+        double INTEREST_RATE = 0.17;
+        double finalValue = 0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        finalValue = creditCardBalance * INTEREST_RATE;
+        System.out.println("The interest due after one month is $" + finalValue);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        finalValue = (finalValue + creditCardBalance)* INTEREST_RATE + finalValue ;
+        System.out.println("The interest due after two months is $" + finalValue);
     }
 }
